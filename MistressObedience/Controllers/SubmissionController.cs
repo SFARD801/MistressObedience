b0f29c944dc12d9d45127898b8ad8c02379617a6
Sadra <Sadra@Home>
2025-03-26 02:22:19 +0330

    Add Submission feature with controller and views
    Created SubmissionController with Serve action method.
    Added Serve Razor page with a service-oriented message.
    Defined ServeModel class for handling GET requests.

---
+++ MistressObedience/Controllers/SubmissionController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MistressObedience.Controllers
+{
+    public class SubmissionController : Controller
+    {
+        public IActionResult Serve()
+        {
+            return View();
+        }
+    }
+}
---
+++ MistressObedience/Views/Submission/Serve.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MistressObedience.Views.Submission
+{
+    public class ServeModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
