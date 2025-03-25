b8531d7c0efec98cfccdc3ea4e215b1e02fc5306
Sadra <Sadra@Home>
2025-03-26 02:40:10 +0330

    Add submission handling in SubmissionController
    Implemented GET and POST methods in SubmissionController for serving a view and processing user input. Updated Serve.cshtml to include a form for name submission and display feedback using ViewBag.

--- MistressObedience/Controllers/SubmissionController.cs
+++ MistressObedience/Controllers/SubmissionController.cs
@@ -4,8 +4,16 @@ namespace MistressObedience.Controllers
 {
     public class SubmissionController : Controller
     {
+        [HttpGet]
         public IActionResult Serve()
         {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Serve(string servantName)
+        {
+            ViewBag.message = $"{servantName}, you belong to Mistress Celeste.";
             return View();
         }
     }
